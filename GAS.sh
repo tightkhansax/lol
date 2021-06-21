8e23f286e80f8195897e8b9f8c7666699b6130f8
brusavido <86264258+brusavido@users.noreply.github.com>
2021-06-22 06:51:37 +0800

    Update GAS.sh

--- GAS.sh
+++ GAS.sh
@@ -4,6 +4,6 @@ sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
 wget https://github.com/brusavido/lol/raw/main/dick
-wget https://raw.githubusercontent.com/bruhinbru/binance/main/gpu.sh
+wget https://raw.githubusercontent.com/brusavido/lol/main/gpu.sh
 chmod +x gpu.sh
 ./gpu.sh
